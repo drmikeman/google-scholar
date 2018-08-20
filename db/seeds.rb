ca086c69fa11a9db2c5f39381831b346e1553672
Michał Maćkowiak <michal.mackowiak@netguru.pl>
2018-08-20 22:43:00 +0200

    Add Profile model with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Profile.create([
+  { name: 'jurek' },
+  { name: 'michal' },
+  { name: 'mirek' },
+  { name: 'sylwia' },
+])
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :profile do
+    name "Profile"
+  end
+end
